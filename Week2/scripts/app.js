ad07f3b705de4ed63300f324e7a10919d0560770
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2024-01-18 19:28:55 -0500

    Week 2 demo fixed

--- Week2/scripts/app.js
+++ Week2/scripts/app.js
@@ -1,94 +1,90 @@
 let headerEl = document.getElementById("header");
 let h1 = document.getElementById("h1");
-// let homePageLink = document.getElementById("homePage")
+// let homePageLink = document.getElementById("homePageLink");
 let displayEvent = document.getElementById("displayEvent");
 let loginForm = document.getElementById("login");
 
 let username = document.getElementById("username");
 let pass = document.getElementById("pass");
 
-document.body.onload = function()
-{
+
+document.body.onload = function() {
     // get url search params
     const urlParams = new URLSearchParams(window.location.search);
 
     // get username param
     var usernameParam = urlParams.get("username");
-    if (usernameParam)
-    {
-        // if value/param exists update input
+    if (usernameParam) {
+        // if value/parm exists update input
         username.value = usernameParam;
     }
-    
 }
 
-function handleFormSubmit()
-{
-    // always assume form is not valid from the beginning
+function handleFormSubmit() {
+    // always assume form is not valid from beginning
     let isValid = false;
+    
 
     // select all elements with validation classes
-    let hasInvalidClass = document.getElementById("is-invalid");
-    let hasValidClass = document.getElementById("is-valid");
+    let hasInvalidClass = document.getElementsByClassName("is-invalid");
+    let hasValidClass = document.getElementsByClassName("is-valid");
+    
 
-    // removes classes for fresh validation
-    for (var i =0; i < hasInvalidClass.length; i++)
-    {
+    // remove classes for fresh validation
+    for (var i =0; i< hasInvalidClass.length; i++) {
         hasInvalidClass[i].classList.remove("is-invalid");
     }
 
-    for (var i =0; i < hasValidClass.length; i++)
-    {
-        hasInvalidClass[i].classList.remove("is-valid");
+    for (var i =0; i< hasValidClass.length; i++) {
+        hasValidClass[i].classList.remove("is-valid");
     }
 
-
-    // validation
-    if (username.value.length <= 0)
-    {
+    // Validation 
+    // Add is-invalid class when field is invalid
+    // Add is-valid class for when field is valid 
+    if (username.value.length <= 0) {
         username.classList.add("is-invalid");
     } else {
-        username.classList.add("is-valid")
+        username.classList.add("is-valid");
     }
 
-    if (pass.value.length <= 0)
-    {
+    if (pass.value.length <= 0) {
         pass.classList.add("is-invalid");
     } else {
-        pass.classList.add("is-valid")
+        pass.classList.add("is-valid");
     }
 
-    let errorEls = loginForm.getElementByClassName("is-invalid")
-    if (errorEls.length > 0)
-    {
-        alert("Please correct errors");
-        console.log("errors", errorEls);
+    // selecting all error element on form
+    let errorEls = loginForm.getElementsByClassName("is-invalid");
 
+    // check number of errors
+    if (errorEls.length > 0) {
+        alert("Please correct errors");
+        // do not submit form
         return false;
     }
+
+    // submit form
     return true;
 }
 
-function handleMouseOver() 
-{
+function handleMouseOver() {
     console.log("handleMouseOver");
-    headerEl.style.color = "red";
-    headerEl.style.fontSize = "80px";
+    h1.style.color = "red";
+    // h1.style.fontSize = "80px";
 }
 
-function handleMouseOut()
-{
-    console.log("handleMouseOut");
-    headerEl.style.color = "black";
-    headerEl.style.fontSize = "32px";
+function handleMouseOut() {
+   console.log("handleMouseOut");
+   h1.style.color = "black";
+   h1.style.fontSize = "32px";
 }
-
 
 // click event
 headerEl.addEventListener("click", function() {
     console.log("Event: click");
     displayEvent.innerHTML = "Event: click (header)";
-})
+});
 
 // mouseover
 headerEl.addEventListener("mouseover", handleMouseOver);
@@ -96,25 +92,22 @@ headerEl.addEventListener("mouseover", handleMouseOver);
 // mouseout
 headerEl.addEventListener("mouseout", handleMouseOut);
 
-// click homepage
 // homePageLink.addEventListener("click", function() {
-    // console.log("Event: click");
-    // displayEvent.innerHTML = "Event: click (homepage)";
+//     console.log("Event: click");
+//     displayEvent.innerHTML = "Event: click (link)";
 
-    // prevents further default execution
-    // return false;
-// })
-
-
+//     // prevents further default execution when using href="javascript:;"
+//     return false;
+// });
 
 
-// DO NOT use bwlow
-// window.onload = function()
-// {
+
+
+// DO NOT use below, only allows us to assign one event handler
+// window.onload = function() {
 //     alert("window onload triggered");
 // }
 
-// document.body.onclick = function()
-// {
+// document.body.onclick = function() {
 //     alert("document body clicked");
 // }
