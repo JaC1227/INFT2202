8518c2e1d0c91ddd06bdb9606fa151204f24aa73
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2024-02-06 18:02:33 -0500

    Week 5 RegExp

---
+++ Week5/scripts/app.js
@@ -0,0 +1,32 @@
+// define parrern for url protocol
+// let urlProtocol = /https/;
+
+// create using constructor
+let urlProtocol = new RegExp("https");
+let url = "http://www.durhamcollege.ca";
+let searchResult = url.search(urlProtocol);
+// returns -1
+console.log(`url =${url} searchResult = ${searchResult}`)
+
+url = "https://www.durhamcollege.ca";
+searchResult = url.search(urlProtocol);
+console.log(`url =${url} searchResult = ${searchResult}`)
+
+/**
+ * 
+ * @param {*} email 
+ */
+function validateEmail(email)
+{
+    let emailPattern = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-z]{2,6})$/;
+    let result;
+    if (emailPattern.test(email)) 
+    {
+        result = true;
+    } 
+    else 
+    {
+        result = false;
+    }
+    // value of result is true
+}
